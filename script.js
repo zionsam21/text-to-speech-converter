115dbfd97594bcd20fc8301a4482ffc075310487
zionsam21 <zionsamuel216@gmail.com>
2026-01-08 15:44:20 +0300

    Initial commit - Text to Speech App

---
+++ script.js
@@ -0,0 +1,26 @@
+const textarea = document.querySelector("textarea");
+const voiceSelect = document.querySelector("select");
+const speakBtn = document.getElementById("speakBtn");
+
+
+let speech = new SpeechSynthesisUtterance();
+
+let voice = [];
+
+let voiceselect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voice = window.speechSynthesis.getVoices();
+    speech.voice = voice[0];
+
+    voice.forEach((voice, i) => (voiceselect.options[i] = new Option(voice.name, i)));
+};
+
+voiceselect.addEventListener("change", () =>{
+    speech.voice = voice[voiceselect.value];
+});
+
+document.querySelector("button").addEventListener("click", () =>{
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
